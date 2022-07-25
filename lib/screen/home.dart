ada3c8675a08f75e6943a60e514e790a05b39608
Suyash <suyashgupta374@gmail.com>
2022-07-26 03:26:21 +0530

    updated home

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class FirstScreen extends StatefulWidget {
   const FirstScreen({Key? key}) : super(key: key);
@@ -12,53 +13,68 @@ class _FirstScreenState extends State<FirstScreen> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      themeMode: ThemeMode.system,
-      darkTheme: ThemeData.dark(),
+        themeMode: ThemeMode.system,
+        darkTheme: ThemeData.dark(),
       home: Scaffold(
-        bottomNavigationBar: NavigationBarTheme(
-          data: NavigationBarThemeData(
-            backgroundColor: const Color(0xff1C202C),
-            indicatorColor: Colors.white54,
-            iconTheme: MaterialStateProperty.resolveWith(
-                  (states) {
-                if (states.contains(MaterialState.selected)) {
-                  return const IconThemeData(color: Color(0xff48365A));
-                } else {
-                  return const IconThemeData(
-                    color: Color(0xff5F6063),
-                  );
-                }
-              },
-            ),
-          ),
-          child: NavigationBar(
-            onDestinationSelected: (int index) {
-              setState(() {
-                currentIndex = index;
-              });
-            },
-            selectedIndex: currentIndex,
-            labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
-            destinations: const <Widget>[
-              NavigationDestination(
-                  icon: Icon(
-                    Icons.home,
-                  ),
-                  label: 'Home'),
-              NavigationDestination(
-                  icon: Icon(
-                    Icons.add,
-                    size: 28,
-                  ),
-                  label: ''),
-              NavigationDestination(
-                  icon: Icon(
-                    Icons.person,
-                  ),
-                  label: 'Profile')
-            ],
-          ),
+        bottomNavigationBar: buildNavigationBarTheme(),
+        appBar: AppBar(
+          shadowColor: Colors.transparent,
+         // backgroundColor: Colors.transparent,
+          elevation: 0,
+          shape: const OutlineInputBorder( borderRadius: BorderRadius.all(Radius.circular(10)),
+              borderSide: BorderSide.none),
+          title:  TextField(
+            decoration: InputDecoration(
+              prefixIcon: IconButton(icon: const FaIcon(FontAwesomeIcons.bars),
+                onPressed: () {  },
+              ),
+              hintText: 'Search',
+              fillColor: Colors.red,
+              // border: const OutlineInputBorder(
+              //     borderRadius: BorderRadius.all(Radius.circular(10)),
+              //     borderSide: BorderSide.none),
+            ),),
+        ),)
+
+    );
+  }
+  NavigationBarTheme buildNavigationBarTheme() {
+    return NavigationBarTheme(
+      data: NavigationBarThemeData(
+        backgroundColor: const Color(0xff1C202C),
+        indicatorColor: Colors.white54,
+        iconTheme: MaterialStateProperty.resolveWith(
+              (states) {
+            if (states.contains(MaterialState.selected)) {
+              return const IconThemeData(color: Color(0xff48365A));
+            } else {
+              return const IconThemeData(
+                color: Color(0xff5F6063),
+              );
+            }
+          },
         ),
+      ),
+      child: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        selectedIndex: currentIndex,
+        labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+        destinations: const <Widget>[
+          NavigationDestination(
+              icon: Icon(
+                Icons.home,
+              ),
+              label: 'Home'),
+          NavigationDestination(
+              icon: Icon(
+                Icons.person,
+              ),
+              label: 'Profile')
+        ],
       ),
     );
   }
--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -30,6 +30,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   AnimatedTextKit(
+                    repeatForever: true,
                     animatedTexts: [
                       ColorizeAnimatedText('Hello There.',
                           textStyle: const TextStyle(
