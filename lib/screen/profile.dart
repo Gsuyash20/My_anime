76bc7aeacd7683d03b9609fe100cd1230d1c2e00
Suyash <suyashgupta374@gmail.com>
2022-07-30 03:57:25 +0530

    profile structure

--- lib/screen/profile.dart
+++ lib/screen/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -10,8 +11,42 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return Theme(data: ThemeData.dark(), child: Scaffold(
-      appBar: AppBar(),
-    ));
+    return Theme(
+        data: ThemeData.dark(),
+        child: Scaffold(
+          body: CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                automaticallyImplyLeading: false,
+                expandedHeight: 240,
+                title: const Text(
+                  'Profile',
+                  style: TextStyle(fontSize: 28,
+                  fontFamily: 'Pacifico'),
+                ),
+                centerTitle: true,
+                flexibleSpace: const FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: CircleAvatar(
+                    radius: 50,
+                  ),
+                ),
+                bottom: PreferredSize(
+                  preferredSize: const Size.fromHeight(60),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      IconButton(
+                        alignment: Alignment.topCenter,
+                        onPressed: () {},
+                        icon: const FaIcon(FontAwesomeIcons.pen),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
